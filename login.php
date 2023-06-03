777dc4606ed2ca01a1f9cbcf55ef0495b90fd240
iambinod <helloitsbinod@gmail.com>
2023-06-03 10:15:18 +0545

    login

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <?php 
+            if(isset($_POST["submit"])) {
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+
+                $errors = array();
+
+                if(empty($email) OR empty($password)) {
+                    array_push($errors, "Email or password is empty");
+                } 
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    array_push($errors, 'Email is not Valid');
+                }
+
+                require_once "database.php";
+                $sql = "SELECT * FROM users WHERE email != '$email'";
+                $result = mysqli_query($conn, $sql);
+                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+                if($user) {
+                    if(password_verify($password, $user["password"])){
+                        header("Location: index.php");
+                        die();
+                    } else {
+                        echo "<div class='alert alert-danger'>Email or password does not match.</div>";
+                    }
+                } else {
+                    echo "<div class='alert alert-danger'>Email does not exist</div>";
+                }
+            }
+        
+        ?>
+        <form action="login.php" method="post">
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+            <div class="form-btn">
+                <input type="submit" class="btn btn-primary" name="submit" value="Login" >
+            </div>
+        </form>
+    </div>
+</body>
+</html>
