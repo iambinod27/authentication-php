e3a8ba079efdc99eae32d72791d6008c120b6785
iambinod <helloitsbinod@gmail.com>
2023-06-03 10:38:43 +0545

    logout implement

--- login.php
+++ login.php
@@ -25,21 +25,13 @@
 
                 $errors = array();
 
-                if(empty($email) OR empty($password)) {
-                    array_push($errors, "Email or password is empty");
-                } 
-
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    array_push($errors, 'Email is not Valid');
-                }
-
                 require_once "database.php";
-                $sql = "SELECT * FROM users WHERE email != '$email'";
+                $sql = "SELECT * FROM users WHERE email = '$email'";
                 $result = mysqli_query($conn, $sql);
                 $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
                 if($user) {
-                    if(password_verify($password, $user["password"])){
+                    if( password_verify($password, $user["password"])){
                         session_start();
                         $_SESSION['user']='yes';
                         header("Location: index.php");
